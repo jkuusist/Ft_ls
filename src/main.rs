9ab66d05d471fa42685df3bb05ddd662201db5a7
jkuusist <jkuusist@student.hive.fi>
2020-12-15 12:56:50 +0100

    added logic to print the filenames in alphabetical order. also made the print formatting nicer with evenly spaced columns

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::{fs, env};
 
+fn longest_len(v: &Vec<String>) -> usize {
+	let mut result = v[0].len();
+
+	for element in v.iter() {
+		if element.len() > result {
+			result = element.len();
+		}
+	}
+
+	result
+}
+
 fn main() {
 	let mut args = env::args();
-
 	args.next();
+
+	let mut v = vec![];
 
 	let path = match args.next() {
 		Some(arg) => arg,
@@ -11,13 +24,29 @@ fn main() {
 	};
 
 	if let Ok(entries) = fs::read_dir(path) {
+
 		for entry in entries {
 			if let Ok(entry) = entry {
-				print!("{} ", entry.file_name().into_string().unwrap());
+				v.push(entry.file_name().into_string().unwrap());
 			} else {
 				print!("Error.")
 			}
 		}
-		print!("\n");
 	}
+
+	let width = longest_len(&v) + 1;
+
+	v.sort_unstable();
+
+	let mut i = 0;
+
+	for filename in v {
+		if i != 0 && i % 6 == 0 {print!("\n");}
+
+		print!("{:-1$}", filename, width);
+
+		i += 1;
+	}
+
+	print!("\n");
 }
