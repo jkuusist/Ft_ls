5c2f9de602142f32ea07b8d78e84c5f6df6602d5
jkuusist <jkuusist@student.hive.fi>
2020-12-16 13:35:54 +0100

    added logic for taking command line flags, as well as a struct to hold which flags are in use

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
+#![allow(non_snake_case)]
 use std::{fs, env};
+
+struct Flags {
+	l_flag: bool,
+	R_flag: bool,
+	a_flag: bool,
+	r_flag: bool,
+	t_flag: bool,
+}
+
+impl Flags {
+	fn new() -> Flags {
+		Flags {
+			l_flag: false,
+			R_flag: false,
+			a_flag: false,
+			r_flag: false,
+			t_flag: false,
+		}
+	}
+
+	fn toggle_on(&mut self, flag_name: char) {
+		match flag_name {
+			'l' => { self.l_flag = true; }
+			'R' => { self.R_flag = true; }
+			'a' => { self.a_flag = true; }
+			'r' => { self.r_flag = true; }
+			't' => { self.t_flag = true; }
+			_ => {panic!("Invalid option");}
+		}
+	}
+}
 
 fn longest_len(v: &Vec<String>) -> usize {
 	let mut result = v[0].len();
@@ -25,18 +57,26 @@ fn print_filenames(v: &Vec<String>, width: usize) {
 }
 
 fn main() {
+	let mut path = String::from(".");
+	let mut flags = Flags::new();
 	let mut args = env::args();
 	args.next();
 
+	for arg in &mut args {
+		if arg.starts_with('-') {
+			for char in arg.chars() {
+				if char != '-' {
+					flags.toggle_on(char);
+				}
+			}
+		} else {
+			path = arg;
+		}
+	}
+
 	let mut v = vec![];
 
-	let path = match args.next() {
-		Some(arg) => arg,
-		None => String::from("."),
-	};
-
 	if let Ok(entries) = fs::read_dir(path) {
-
 		for entry in entries {
 			if let Ok(entry) = entry {
 				v.push(entry.file_name().into_string().unwrap());
